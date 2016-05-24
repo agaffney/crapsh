06804c4a7a00e9ab087fec9a5c0cd1ccdd73ae04
Andrew Gaffney <andrew@agaffney.org>
2016-05-23 21:34:46 -0600

    strip dir off of binary name

--- cmd/main.go
+++ cmd/main.go
@@ -2,18 +2,13 @@ package cmd
 
 import (
 	"github.com/agaffney/crapsh/core"
-	//"github.com/agaffney/crapsh/prompt"
 	"os"
+	"path"
 )
 
 func Start() {
-	//fmt.Println("in main()")
-	//p := &prompt.Prompt{
-	//	Text: "prompt text",
-	//}
-	//p.Show()
 	c := &core.Config{}
-	c.Binary = os.Args[0]
+	c.Binary = path.Base(os.Args[0])
 	parse_cmdline_opts(c)
 	state := core.New(c)
 	state.Start()
