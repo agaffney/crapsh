00306d0d926d7da48cc766e9a3143dc3d3940619
Andrew Gaffney <andrew@agaffney.org>
2016-05-27 19:56:03 -0600

    fix test

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -1,6 +1,9 @@
 package parser
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestParser(t *testing.T) {
 	parser := NewParser()
@@ -8,6 +11,6 @@ func TestParser(t *testing.T) {
 		"foo $(echo bar foo bar) baz\nabc \"123 456\" 'd\nef' 789",
 	}
 	for _, input := range test_inputs {
-		parser.Parse(input)
+		parser.Parse(strings.NewReader(input))
 	}
 }
