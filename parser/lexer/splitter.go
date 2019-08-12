dfdf7023f63e3d62c993e1afccc28af7b0f6d431
Andrew Gaffney <andrew@agaffney.org>
2019-08-11 22:14:58 -0500

    parse operators per the spec

--- parser/lexer/splitter.go
+++ parser/lexer/splitter.go
@@ -1,14 +1,45 @@
 package lexer
 
 import (
-	"bytes"
+	//"bytes"
+	"fmt"
 	"github.com/agaffney/crapsh/parser/rules"
 	"io"
 )
 
+func (l *Lexer) checkForOperator(value string, startsWith bool) int {
+	for _, op := range rules.OperatorRules {
+		var opLen int
+		if startsWith {
+			// Use the smaller of the value length and operator length
+			opLen = len(value)
+			if len(op.Pattern) < len(value) {
+				opLen = len(op.Pattern)
+			}
+		} else {
+			// Use the operator length, since we're looking for an exact match
+			opLen = len(op.Pattern)
+			// Move to the next operator candidate if our input is a different length than the operator
+			if len(value) != opLen {
+				continue
+			}
+		}
+		// Return operator token type if there's a match
+		if value[:opLen] == op.Pattern[:opLen] {
+			return op.TokenType
+		}
+	}
+	return -1
+}
+
+// Tokenize the input per the POSIX spec
+// https://pubs.opengroup.org/onlinepubs/9699919799/utilities/V3_chap02.html#tag_18_03
 func (l *Lexer) NextToken() (*Token, error) {
 	token := &Token{}
 	delimRuleStack := []*rules.DelimeterRule{rules.GetDelimeterRule(`Word`)}
+	processingOperator := false
+	//processingDelimeter := false
+	//escapeFound := false
 	for {
 		// Reset token line/offset if there's no value yet
 		if len(token.Value) == 0 {
@@ -16,25 +47,10 @@ func (l *Lexer) NextToken() (*Token, error) {
 			token.Offset = l.lineOffset
 		}
 		curDelimRule := delimRuleStack[len(delimRuleStack)-1]
-		buf_string := l.buf.String()
-		if curDelimRule.AllowOperators {
-			for _, op := range rules.OperatorRules {
-				if len(buf_string) >= len(op.Pattern) && buf_string[:len(op.Pattern)] == op.Pattern {
-					if len(token.Value) > 0 {
-						return token, nil
-					}
-					token.Value = op.Pattern
-					token.Type = op.TokenType
-					// Remove operator from buffer
-					l.buf = bytes.NewBufferString(buf_string[len(op.Pattern):])
-					l.lineOffset += len(op.Pattern)
-					return token, nil
-				}
-			}
-		}
 		c, err := l.nextRune()
 		if err != nil {
 			if err == io.EOF {
+				// Return the current token, if any
 				if len(token.Value) > 0 {
 					return token, err
 				} else {
@@ -44,18 +60,53 @@ func (l *Lexer) NextToken() (*Token, error) {
 				return nil, err
 			}
 		}
+		if curDelimRule.AllowOperators {
+			if !processingOperator {
+				// Check if current rune starts an operator
+				if tokenType := l.checkForOperator(string(c), true); tokenType != -1 {
+					if len(token.Value) > 0 {
+						// Return rune to the buffer
+						l.unreadRune()
+						return token, nil
+					}
+					processingOperator = true
+				}
+			}
+		}
 		token.Value += string(c)
+		if processingOperator {
+			// Check if current token value still matches an operator
+			tokenType := l.checkForOperator(token.Value, false)
+			if tokenType == -1 {
+				// Return last rune to the buffer and return operator token
+				l.unreadRune()
+				token.Value = token.Value[:len(token.Value)-1]
+				return token, nil
+			} else {
+				// Update token type with current best guess for operator
+				token.Type = tokenType
+			}
+		}
+		/*
+			if !curDelimRule.IgnoreEscapes && c == '\\' {
+				escapeFound = !escapeFound
+				continue
+			}
+		*/
 		ruleMatched := false
+		// TODO: check for opening delimeter by start character like with operators
 		for _, ruleName := range curDelimRule.AllowedRules {
 			rule := rules.GetDelimeterRule(ruleName)
 			if len(token.Value) >= len(rule.DelimStart) && token.Value[len(token.Value)-len(rule.DelimStart):] == rule.DelimStart {
 				// Add delimeter rule to stack
 				delimRuleStack = append(delimRuleStack, rule)
 				ruleMatched = true
+				fmt.Printf("delimeter rule start: token.Value = '%s', rule = %#v\n", token.Value, rule)
 				break
 			}
 		}
 		if ruleMatched {
+			//escapeFound = false
 			continue
 		}
 		if len(token.Value) >= len(curDelimRule.DelimEnd) && string(token.Value[len(token.Value)-len(curDelimRule.DelimEnd)]) == curDelimRule.DelimEnd {
@@ -69,10 +120,12 @@ func (l *Lexer) NextToken() (*Token, error) {
 				if len(token.Value) > 0 {
 					return token, nil
 				}
+				//escapeFound = true
 				continue
 			}
 			// Remove current delimeter rule from the stack
 			delimRuleStack = delimRuleStack[:len(delimRuleStack)-1]
+			fmt.Printf("delimeter rule end: token.Value = '%s', rule = %#v\n", token.Value, curDelimRule)
 		}
 	}
 	return token, nil
