e3ed4bdd8d3c37176e7fb3e7f6c930be3dbf1d3b
Andrew Gaffney <andrew@agaffney.org>
2019-08-17 21:07:52 -0500

    flesh out delimeter rules

--- parser/rules/delimeters.go
+++ parser/rules/delimeters.go
@@ -1,14 +1,14 @@
 package rules
 
 type DelimeterRule struct {
-	Name           string
-	DelimStart     string
-	DelimEnd       string
-	AllowedRules   []string
-	ReturnToken    bool
-	IgnoreEscapes  bool
-	IncludeDelim   bool
-	AllowOperators bool
+	Name           string   // rule name
+	DelimStart     string   // starting delimeter
+	DelimEnd       string   // ending delimeter
+	AllowedRules   []string // delimeter rule names that are allowed inside this delimeter set
+	ReturnToken    bool     // whether to return a token when finding the end delimeter
+	IgnoreEscapes  bool     // whether to ignore escape sequences
+	IncludeDelim   bool     // whether to include the delimeters in the token
+	AllowOperators bool     // whether to allow operators
 }
 
 var DelimeterRules = []DelimeterRule{
@@ -19,11 +19,40 @@ var DelimeterRules = []DelimeterRule{
 		AllowOperators: true,
 		AllowedRules:   []string{`SingleQuotes`, `DoubleQuotes`, `Subshell`, `SubshellBackticks`, `Arithmetic`},
 	},
-	{Name: `SingleQuotes`, DelimStart: `'`, DelimEnd: `'`, IgnoreEscapes: true, IncludeDelim: true},
-	{Name: `DoubleQuotes`, DelimStart: `"`, DelimEnd: `"`, IncludeDelim: true, AllowedRules: []string{}},
-	{Name: `Subshell`, DelimStart: `$(`, DelimEnd: `)`, IncludeDelim: true, AllowedRules: []string{}},
-	{Name: `SubshellBackticks`, DelimStart: "`", DelimEnd: "`", IncludeDelim: true, AllowedRules: []string{}},
-	{Name: `Arithmetic`, DelimStart: `$((`, DelimEnd: `))`, IncludeDelim: true},
+	{
+		Name:          `SingleQuotes`,
+		DelimStart:    `'`,
+		DelimEnd:      `'`,
+		IgnoreEscapes: true,
+		IncludeDelim:  true,
+	},
+	{
+		Name:         `DoubleQuotes`,
+		DelimStart:   `"`,
+		DelimEnd:     `"`,
+		IncludeDelim: true,
+		AllowedRules: []string{`Subshell`, `SubshellBackticks`, `Arithmetic`},
+	},
+	{
+		Name:         `Subshell`,
+		DelimStart:   `$(`,
+		DelimEnd:     `)`,
+		IncludeDelim: true,
+		AllowedRules: []string{`SingleQuotes`, `DoubleQuotes`, `Subshell`, `SubshellBackticks`, `Arithmetic`},
+	},
+	{
+		Name:         `SubshellBackticks`,
+		DelimStart:   "`",
+		DelimEnd:     "`",
+		IncludeDelim: true,
+		AllowedRules: []string{`SingleQuotes`, `DoubleQuotes`, `Subshell`, `Arithmetic`},
+	},
+	{
+		Name:         `Arithmetic`,
+		DelimStart:   `$((`,
+		DelimEnd:     `))`,
+		IncludeDelim: true,
+	},
 }
 
 func GetDelimeterRule(name string) *DelimeterRule {
